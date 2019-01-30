98030136953252a53906fd79ad96347f7a61662b
sarakinzbruner <36466804+sarakinzbruner@users.noreply.github.com>
2019-01-30 14:05:22 -0500

    bootcamp finished

--- listingController.js
+++ listingController.js
@@ -7,8 +7,24 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+		if (angular.isDefined($scope.name) && $scope.name != '' && $scope.code != '') 
+                {
+					var c = {'latitude': $scope.latitude, 'longitude': $scope.longitude}
+                    $scope.listings.push({ code: $scope.code, name: $scope.name,coordinates: c, address: $scope.address});
+                    
+					$scope.name = '';
+                    $scope.code = '';
+					$scope.latitude = '';
+					$scope.longitude = '';
+					$scope.address = '';
+                }
+	};
+    $scope.deleteListing = function(index) {
+		$scope.listings.splice($scope.listings.indexOf(index), 1);
+	};
+    $scope.showDetails = function(index) {
+		$scope.detailedInfo = $scope.listings[$scope.listings.indexOf(index)];
+	};
   }
 ]);
